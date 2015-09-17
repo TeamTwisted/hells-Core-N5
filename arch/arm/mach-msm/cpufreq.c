8dce9c7a77fe760a77fc5b9f590cd043493c4f74
aow1980 <wilkes.allen@gmail.com>
2015-09-16 23:08:02 -0400

    Raise stock Min to 300mhz

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -272,7 +272,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 		cpumask_setall(policy->cpus);
 
 	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
-		policy->cpuinfo.min_freq = 192000;
+		policy->cpuinfo.min_freq = 300000;
 		policy->cpuinfo.max_freq = 2265600;
 	}
 
